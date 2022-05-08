14900c2d112e67687abd4805a0b0599e3f45a3df
Alex Silva <alexcssilva@gmail.com>
2022-05-08 14:19:18 -0300

    feat: added product controller

---
+++ controllers/productController.js
@@ -0,0 +1,18 @@
+const productService = require('../services/productService');
+
+const listProducts = async (req, res, _next) => {
+    const [products] = await productService.getProduct();
+    return res.status(200).json(products);
+};
+
+const listIdProducts = async (req, res, _next) => {
+  const { id } = req.params;
+    const [productId] = await productService.getIdProduct(id);
+    if (!productId) return res.status(404).json({ message: 'Product not found' });
+    return res.status(200).json(productId);
+};
+
+module.exports = {
+  listProducts,
+  listIdProducts,
+};
