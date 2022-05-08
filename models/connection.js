83ffc5862aa1aa6c87afa045848ca2079099ef0e
Alex Silva <alexcssilva@gmail.com>
2022-05-08 19:17:59 -0300

    fix: Desafio 02 Concluido

--- models/connection.js
+++ models/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); 
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || '127.0.0.1',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'trybe',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
