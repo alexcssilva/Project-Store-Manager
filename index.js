a5d689336b870c536b1e317e5a7711ae9d14ab41
Alex Silva <alexcssilva@gmail.com>
2022-05-08 14:21:05 -0300

    feat: added product router index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const app = require('./app');
 require('dotenv').config();
+const router = require('./router/index');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
