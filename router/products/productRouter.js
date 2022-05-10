6188a94cbc34182dad61398ee505b8b96768b9cc
Alex Silva <alexcssilva@gmail.com>
2022-05-09 22:39:46 -0300

    feat: update productModel with produtct.post

--- router/products/productRouter.js
+++ router/products/productRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const productController = require('../../controllers/productController');
+const { compareName } = require('../../middlewares/validateProduct');
 
 const productRouter = express.Router();
 
 productRouter.get('/', productController.listProducts);
 productRouter.get('/:id', productController.listIdProducts);
+
+productRouter.post('/', compareName, productController.createNewProduct);
 
 module.exports = productRouter;
