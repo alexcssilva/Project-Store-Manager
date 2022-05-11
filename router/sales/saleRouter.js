ac4848d031c6df1d6493c5fe3b8b4c7ff81c1b06
Alex Silva <alexcssilva@gmail.com>
2022-05-11 19:02:43 -0300

    feat: update saleRouter Services

--- router/sales/saleRouter.js
+++ router/sales/saleRouter.js
@@ -6,4 +6,6 @@ const saleRouter = express.Router();
 saleRouter.get('/', saleController.listSales);
 saleRouter.get('/:id', saleController.listIdSales);
 
+saleRouter.post('/', saleController.createNewSale);
+
 module.exports = saleRouter;
