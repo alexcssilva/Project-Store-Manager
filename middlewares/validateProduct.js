857cfe9d4d0e2f1351ecc9b1ded150eecd71d30b
Alex Silva <alexcssilva@gmail.com>
2022-05-11 23:58:27 -0300

    fix: remove console.log validateProduct

--- middlewares/validateProduct.js
+++ middlewares/validateProduct.js
@@ -12,7 +12,6 @@ const compareName = async (req, res, next) => {
 
 const validateName = async (req, res, next) => {
   const { name } = req.body;
-  console.log('; req.body', req.body);
 
   if (!name) {
     return res.status(400).json({ message: '"name" is required' });
