f2ecd7cf6bd1f74e9acb175170f0552c286eb1ee
Alex Silva <alexcssilva@gmail.com>
2022-05-11 23:50:55 -0300

    feat: added ValidateName at productRouter

--- router/products/productRouter.js
+++ router/products/productRouter.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const productController = require('../../controllers/productController');
-const { compareName, validateName, lengthName } = require('../../middlewares/validateProduct');
+const { 
+  compareName,
+  validateName, 
+  lengthName, 
+  validateQuantity,
+} = require('../../middlewares/validateProduct');
 
 const productRouter = express.Router();
 
 productRouter.get('/', productController.listProducts);
 productRouter.get('/:id', productController.listIdProducts);
 
-productRouter.post('/', validateName, lengthName, compareName, productController.createNewProduct);
+productRouter.post('/', 
+  validateName, 
+  validateQuantity,
+  lengthName, 
+  compareName,
+  productController.createNewProduct);
 
 productRouter.put('/:id', productController.idProducts, productController.updateProduct);
 
