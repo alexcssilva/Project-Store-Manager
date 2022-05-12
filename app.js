b51fca6f0243b133ca3bab8ada1c052fc3ed4ce7
Alex Silva <alexcssilva@gmail.com>
2022-05-11 23:51:50 -0300

    feat: added bodyParser and router - Nao estava passando no teste local

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./router/index');
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(router);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
