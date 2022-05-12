f862243ef50ab81ce9c82da5642fc862efe239fe
Alex Silva <alexcssilva@gmail.com>
2022-05-11 23:24:56 -0300

    feat: added lengthName at productRouter

--- router/products/productRouter.js
+++ router/products/productRouter.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const productController = require('../../controllers/productController');
-const { compareName, validateName } = require('../../middlewares/validateProduct');
+const { compareName, validateName, lengthName } = require('../../middlewares/validateProduct');
 
 const productRouter = express.Router();
 
 productRouter.get('/', productController.listProducts);
 productRouter.get('/:id', productController.listIdProducts);
 
-productRouter.post('/', validateName, compareName, productController.createNewProduct);
+productRouter.post('/', validateName, lengthName, compareName, productController.createNewProduct);
 
 productRouter.put('/:id', productController.idProducts, productController.updateProduct);
 
