a5689f4c1d881b8ac6c2f7642be1b66b35458d3f
Alex Silva <alexcssilva@gmail.com>
2022-05-13 22:33:26 -0300

    feat: added saleServicesGetAll test/unit

---
+++ test/unit/services/productsServicesGetAll.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../models/connection');
+const productModel = require('../../../models/productModel');
+const productService = require('../../../services/productService');
+
+describe('Busca produtos no BD', () => {
+  describe('quando existe produdos', () => {
+
+    const resultExecute = ([])
+
+before(() => {
+  sinon.stub(productModel, 'listAllProducts')
+    .resolves(resultExecute)
+})
+
+after(() => {
+  productModel.listAllProducts.restore()
+})
+
+    it('retorna uma array', async () => {
+      const result = await productService.getProduct()
+
+      expect(result).to.be.an('array')
+    });
+
+    it('o array está vazio',async () => {
+      const result = await productService.getIdProduct()
+
+      expect(result).to.be.empty;
+    });
+  });
+});
